e9d9cf0d7b0d4ca112ce1c2f4f814daccf2dce84
Manuhe <dev.by.manda@gmail.com>
2025-04-02 16:53:19 +0300

    Basic Express server and Env Variables.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('Server is running');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
