06ef9b2d0a84933d666c0c9c1e4797fe360d4630
Manuhe <dev.by.manda@gmail.com>
2025-04-04 21:23:40 +0300

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,6 +34,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -57,6 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -71,6 +73,12 @@ const getMe = asyncHandler(async (req, res) => {
   res.json({ msg: 'User data' });
 });
 
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 module.exports = {
   registerUser,
   loginUser,
