465b7db860e13c25d49daa419f7b4f329b0ce0ea
Manuhe <dev.by.manda@gmail.com>
2025-04-05 12:40:24 +0300

    Initialized frontend with react and redux-toolkit

---
+++ frontend/src/store/store.js
@@ -0,0 +1,8 @@
+// src/store/store.js (JavaScript)
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    // Add your reducers here
+  },
+});
