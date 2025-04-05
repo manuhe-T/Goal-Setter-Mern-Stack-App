9340fc2be3402b4ecffd5623aa55385f37f1f9d5
Manuhe <dev.by.manda@gmail.com>
2025-04-05 08:35:22 +0300

    Update goalController.js

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -15,12 +15,12 @@ const getGoals = asyncHandler(async (req, res) => {
 // @access Private
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
-    return res.status(400);
+    res.status(400);
     throw new Error('Please add a text field');
   }
   const goal = await Goal.create({
-    text: req.body.text,
     user: req.user.id,
+    text: req.body.text,
   });
   res.status(201).json(goal);
 });
