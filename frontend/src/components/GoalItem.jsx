b04ecbfa835b60f698545078c12e2b2610188f9c
Manuhe <dev.by.manda@gmail.com>
2025-04-08 08:32:29 +0300

    Update GoalItem.jsx

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,13 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { deleteGoal } from '../store/goalSlice';
+import { toast } from 'react-toastify';
 
 function GoalItem({ goal }) {
   const dispatch = useDispatch();
+  const handleDeleteGoal = (id) => {
+    dispatch(deleteGoal(id));
+    toast.success('Goal deleted successfully');
+  };
   return (
     <div className="goal">
       <div className="">{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
-      <button className="close" onClick={() => dispatch(deleteGoal(goal._id))}>
+      <button className="close" onClick={() => handleDeleteGoal(goal._id)}>
         X
       </button>
     </div>
