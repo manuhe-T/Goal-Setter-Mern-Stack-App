db8d3bb1045253f2b198b832b609d03d0e3ab221
Manuhe <dev.by.manda@gmail.com>
2025-04-08 08:14:46 +0300

    Added a delete Option in the Goal Item

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { deleteGoal } from '../store/goalSlice';
+
 function GoalItem({ goal }) {
+  const dispatch = useDispatch();
   return (
     <div className="goal">
       <div className="">{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
+      <button className="close" onClick={() => dispatch(deleteGoal(goal._id))}>
+        X
+      </button>
     </div>
   );
 }
